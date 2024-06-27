bf198eb80d90adc564168a33067437b111813727
mrud0004 <mrud0004@student.monash.edu>
2024-06-27 18:07:02 +1000

    Add links to projects

--- personal-website/src/Projects.js
+++ personal-website/src/Projects.js
@@ -3,6 +3,7 @@ import Particle from './components/particles';
 import HeaderComponent from './components/HeaderComponent';
 import './Home.css';
 import ProjectDisplay from './components/ProjectDisplay';
+import { useLinkClickHandler } from 'react-router-dom';
 
 
 function Projects() {
@@ -11,17 +12,20 @@ function Projects() {
         {
           image: "/project1.png",
           title: "The Watchlist",
-          description: "Building a movie watchlist app as part of a student team, using React which allows users to rate and save their favourite movies. "
+          description: "Building a movie watchlist app as part of a student team, using React which allows users to rate and save their favourite movies.",
+          link: "https://github.com/Monash-FIT3170/The-Watchlist"
         },
         {
           image: "/project2.png",
           title: "AI PDF Summariser",
-          description: "A tool designed to summarise PDF documents efficiently, integrating the latest Open AI advancements for streamlined workflow."
+          description: "A tool designed to summarise PDF documents efficiently, integrating the latest Open AI advancements for streamlined workflow.",
+          link: "https://github.com/mrud0004/ai-summariser-pdf"
         },
         {
           image: "/project3.jpg",
           title: "NBA HOF Machine Learning Project",
-          description: "Utilised machine learning to predict NBA Hall of Fame inductions with a Random Forest model."
+          description: "Utilised machine learning to predict NBA Hall of Fame inductions with a Random Forest model.",
+          link: "https://github.com/mrud0004/nba-machine-learn-hof"
         }
       ];
 
@@ -37,6 +41,7 @@ function Projects() {
                 image={project.image}
                 title={project.title}
                 description={project.description}
+                link = {project.link}
               />
             </div>
           ))}
